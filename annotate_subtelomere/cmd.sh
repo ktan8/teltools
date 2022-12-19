ab4dc2def503adbc026e32378f4994458f0b798e
Kar-Tong Tan <ktan@g.harvard.edu>
2022-12-19 11:35:22 -0500

    remove not required package

--- annotate_subtelomere/cmd.sh
+++
@@ -1,2 +0,0 @@
-minimap2 -x asm5 ./data/paralogy.blocks.stong_final.fasta ./data/sample/subtelomere_500kb.cleanName.fasta > map_subtelomere_to_parologyblocks.paf
-minimap2 -x asm5 ./data/paralogy.blocks.stong_final.fasta ./data/sample/chm13.draft_v1.0.edge_500kb.fasta > map_chm13subtelomere_to_parologyblocks.paf
